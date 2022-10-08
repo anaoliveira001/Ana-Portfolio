a3804ccefb4d14b5569dd1c740d0766eb7f5a07c
Ana Paula <ana.oliveira.prt_a@msft.cesae.pt>
2022-10-08 23:40:05 +0100

    Update

--- index.php
+++ index.php
@@ -7,11 +7,49 @@
     <link rel="shortcut icon" href="images/code_Ana1.png" type="image/x-icon">
     <!--CSS file -->
     <link rel="stylesheet" href="css/style.css">
+     <!--SCSS file -->
+     <link rel="stylesheet" href="css/style.scss">
     <!--CND link - font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <title>Ana_Dev</title>
 </head>
 <body>
+<!--Animation Starts -->
+<div id="particle-container">
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+  <div class="particle"></div>
+</div>
+<!--Animation ends -->
+
+
 <!--header starts -->
 <header class="header">
     <div id="menu-btn" class="fas fa-bars"></div>
@@ -41,8 +79,7 @@
         <div class="content">
            <h3>Hi, I am Ana Oliveira</h3>
            <span>Frontend Web Developer and UI/UX Designer</span>
-           <p>I started studying programming when I was 17. 
-                I have a few projects and I want to work in a technology company that allows me to grow and develop my skills together with a team.</p>
+           <p></p>
            <a href="#about" class="btn">About Me</a>
         </div>
     </section>
@@ -53,8 +90,9 @@
 
         <h1 class="heading"><span>Biography</span></h1>
         <div class="biography">
-            <p>Where I studied: <br>
-               IGTI, Alura, Senai, Udemy, Coursera and Microsoft.</p>
+            <p>
+            I started studying programming when I was 17. 
+            I have a few projects and I want to work in a technology company that allows me to grow and develop my skills together with a team.</p>
 
             <div class="bio">
                 <h3><span>Name : </span>Ana Paula Oliveira</h3>
@@ -65,25 +103,186 @@
                 <h3><span>Experince : </span>3+ years</h3>
             </div>
             <a href="images/CV - Ana Oliveira.pdf" class="btn" target="_blank">Download my CV</a>
-        </div>
+        </div> 
 
         <div class="skills">
             <h1 class="heading"><span>Skills</span></h1>
             <div class="progress">
-                <div class="bar"><span>HTML5</span><span>90%</span></div>
-                <div class="bar"><span>CSS3</span><span>80%</span></div>
-                <div class="bar"><span>JavaScript</span><span>65%</span></div>
-                <div class="bar"><span>Bootstrap</span><span>60%</span></div>
-                <div class="bar"><span>VUE</span><span>55%</span></div>
-                <div class="bar"><span>React</span><span>55%</span></div>
-                <div class="bar"><span>Python</span><span>50%</span></div>
-                <div class="bar"><span>Go</span><span>50%</span></div>
+                <div class="bar"><h3><span>HTML5</span> <span>90%</span></h3></div>
+                <div class="bar"><h3><span>CSS3</span> <span>65%</span></h3></div>
+                <div class="bar"><h3><span>JavaScript</span> <span>55%</span></h3></div>
+                <div class="bar"><h3><span>Bootstrap</span> <span>45%</span></h3></div>
+                <div class="bar"><h3><span>VUE</span> <span>45%</span></h3></div>
+                <div class="bar"><h3><span>React</span> <span>45%</span></h3></div>
+                <div class="bar"><h3><span>Python</span> <span>40%</span></h3></div>
+                <div class="bar"><h3><span>Go</span> <span>40%</span></h3></div> <br>
+            
+                <div class="edu-exp">
+                    <h1 class="heading"><span> Education & Experince</span></h1>
+                    <div class="row">
+                        <div class="box-container">
+                            <h3 class="title">Education</h3>
+
+                            <div class="box">
+                                <span>(2007 - 2008)</span>
+                                <h3>Senai - Web Designer</h3>
+                                <p>
+                                    HTML, CSS, Dw, Ps, Ai, Flash, JS
+                                </p>
+                            </div>
+
+                            <div class="box">
+                                <span>(2019-2020)</span>
+                                <h3>Udemy Web Development - Frontend</h3>
+                                <p>
+                                    ES(6), Vue, React
+                                </p>
+                            </div>
+
+                            <div class="box">
+                                <span>(2020)</span>
+                                <h3>Data Analyst - Google Coursera</h3>
+                                <p>
+                                    R langague, Table, Data manipulation, plots
+                                </p>
+                            </div>
+
+                            <div class="box">
+                                <span>(2021)</span>
+                                <h3>UX/UI Desing - Google Coursera</h3>
+                                <p>
+                                   Advanced techniques in UX/UI, personas and accessibility
+                                </p>
+                            </div>
+
+                            <div class="box">
+                                <span>(2021)</span>
+                                <h3>Bootcamp Frontend - IGTI</h3>
+                                <p>
+                                    HTML, CSS, JS, Vue,advanced query handling techniques.
+                                </p>
+                            </div>
+                            <div class="box">
+                                <span>(2021 - 2022)</span>
+                                <h3>Formação Frontent - Alura</h3>
+                                <p>
+                                    HTML, CSS, JS, Vue, Angular, React.
+                                </p>
+                            </div>
+
+                            <div class="box">
+                                <span>(2021 - 2022)</span>
+                                <h3>Formação Python - Microsoft</h3>
+                                <p>
+                                    Fundamentals, Advanced, OPP.
+                                </p>
+                            </div>
+
+                            <div class="box">
+                                <span>(2022-2023)</span>
+                                <h3>Formação Web Development (Frontend/Backend) - Cesae</h3>
+                                <p>
+                                    Java, HTML, CSS, Bootstrap, PHP, Laravel, Angular, Android Mobile
+                                </p>
+                            </div>
+                            <div class="box">
+                                <span>(2022-2023)</span>
+                                <h3>Formação Azure Cloud - Microsoft</h3>
+                                <p>
+                                   Cloud Services
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="box-container">
+                            <h3 class="title">Experince</h3>
+
+                            <div class="box">
+                                <span>(2013 - 2022)</span>
+                                <h3>Web Designer/ Frontend Developer</h3>
+                            </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
 <!--about section end--> 
 
+<!--services section starts--> 
+    <section class="services" id="services">
+        <h1 class="heading"><span>Services</span></h1>
 
+        <div class="box-container">
+
+            <div class="box">
+                <i class="fas fa-code"></i>
+                <h3>Web Development</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere veniam aut aspernatur sint temporibus illo atque laboriosam. Voluptates ipsam modi aliquam corrupti. Voluptate perspiciatis quo cupiditate quia! Neque, perspiciatis!</p>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-paint-brush"></i>
+                <h3>Graphic Desing</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere veniam aut aspernatur sint temporibus illo atque laboriosam. Voluptates ipsam modi aliquam corrupti. Voluptate perspiciatis quo cupiditate quia! Neque, perspiciatis!</p>
+            </div>
+
+            <div class="box">
+                <i class="fab fa-bootstrap"></i>
+                <h3>Bootstrap</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere veniam aut aspernatur sint temporibus illo atque laboriosam. Voluptates ipsam modi aliquam corrupti. Voluptate perspiciatis quo cupiditate quia! Neque, perspiciatis!</p>
+            </div>
+
+            <div class="box">
+                <i class="fab fa-wordpress"></i>
+                <h3>Wordpress</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere veniam aut aspernatur sint temporibus illo atque laboriosam. Voluptates ipsam modi aliquam corrupti. Voluptate perspiciatis quo cupiditate quia! Neque, perspiciatis!</p>
+            </div>
+        </div>
+    </section>
+<!--services section ends--> 
+
+<!--work section starts-->
+
+<section class="portfolio" id="portflolio">
+
+    <h1 class="heading"><span>My Work - Personal Portfolio</span></h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/7.png" alt="AnaDev">
+            <h3>Graphic Desing</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/1.png" alt="Programming">
+            <h3>Web Developer - Tips</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/2.png" alt="HTML Tags">
+            <h3>Web Developer - HTML Tags</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/3.png" alt="CSS - What is it?">
+            <h3>Web Developer - CSS - What is it?</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/4.png" alt="JavaScript - How to use?">
+            <h3>Web Developer - JavaScript - How to use?</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/5.png" alt="Framework and Libraries">
+            <h3>Web Developer - Framework and Libraries</h3>
+        </div>
+    </div>
+</section>
+
+
+<!--work section ends--> 
 
 
 
